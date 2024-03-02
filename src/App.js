8d5190852c41dbe1c4eb075e4c86507433b0c439
Diego <diegoa2992@gmail.com>
2024-03-01 23:52:04 -0600

    restored prev state and added resume to my website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
-// import { TextGenerator, Icon, TextWrapper } from "./components/ui/TextGenerator.tsx";
-// import { DotBackgroundDemo } from './components/ui/grid-background.tsx';
-import { ParentComponent} from "./components/ParentComponent.tsx";
+import { WebsiteManager} from "./components/WebsiteManager.tsx";
 import './output.css'
-
 export function App() {
   return (
-    // <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-screen w-full">
     <>
-    {/* <TextGenerator/> */}
-    {/* <TextWrapper/> */}
-    {/* <DotBackgroundDemo/> */}
-    <ParentComponent/>
+    <WebsiteManager/>
     </>
-    // </div>
-   
-   
-    
-    
   );
 }
 
--- src/components/WebsiteManager.tsx
+++ src/components/WebsiteManager.tsx
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
-import { TextGenerator } from "./ui/TextGenerator.tsx"; // Adjust the import path as necessary
+import { TextGenerator } from "./ui/TextGenerator.tsx"; 
 import {ReactComponent as LinkedInIcon} from '../assets/icons/linkedin.svg';
 import {ReactComponent as GitHubIcon} from '../assets/icons/github1.svg';
+import {Projects} from "./ui/Projects.tsx";
 import { Xterm } from "./ui/Terminal.tsx";
 import '../index.css';
 
 const linkedInProfileUrl = "https://www.linkedin.com/in/diegocoronado0/";
 const gitHubProfileUrl = "https://www.github.com/sudoneoox";
 
-export function ParentComponent() {
+export function WebsiteManager() {
 
     const [isVisible, setIsVisible] = useState({
         TextGenerator: true,
         xterm: false,
-        about: false,
-        contact: false,
         projects:false,
     })
 
@@ -33,7 +32,10 @@ export function ParentComponent() {
         });
     };
     
- 
+    const openResume = () => { 
+
+        window.open('../../assets/Resume.pdf', '_blank');
+    } 
 
     return (
         <>
@@ -47,7 +49,8 @@ export function ParentComponent() {
                 <div className="absolute navigation-links top-0 right-0 m-4" style={{ display: 'flex', gap: '20px'}}>
                 <button onClick={() => showSection('TextGenerator')} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>.about()</button>
                     <button onClick={() => showSection('projects')} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>.projects()</button>
-                <button onClick={() => showSection('contact')} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>.contact()</button> 
+                <button onClick={openResume} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>.resume()</button> 
+                
                     <a href={gitHubProfileUrl} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>
                         <GitHubIcon className="w-6 h-6" />
                     </a>
@@ -61,9 +64,7 @@ export function ParentComponent() {
         
             {isVisible.xterm && <Xterm className="h-[50rem] w-full dark:bg-black bg-black  bg-dot-white/[0.3] relative flex items-center justify-center" />}
             {isVisible.TextGenerator && <TextGenerator className="dark:bg-black bg-dot-white/[0.3]  relative font-bold z-20 text-pretty justify-center"/>}
-            {/* {isVisible.about && <div className="bg-black text-white">About</div>} */}
-            {/* {isVisible.contact && <div className="bg-black text-white">Contact</div>} */}
-            {/* {isVisible.projects && <div className="bg-black text-white">Projects</div>} */}
+            {isVisible.projects && <div className="w-full h-[50rem] flex items-center justify-center"><Projects/></div>}
         </>
     );
 }
