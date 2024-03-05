b443435e12959746beec965c6c2b9ec32fcff902
Diego Arch Desktop <diegoa2992@gmail.com>
2024-03-04 23:53:09 -0600

    added contact section and made websitemanager more readable

--- src/components/WebsiteManager.tsx
+++ src/components/WebsiteManager.tsx
@@ -11,35 +11,33 @@ const gitHubProfileUrl = "https://www.github.com/sudoneoox";
 
 export function WebsiteManager() {
 
-    const [isVisible, setIsVisible] = useState({
-        TextGenerator: true,
-        xterm: false,
-        projects:false,
-    })
+    const [currentSection, setCurrentSection] = useState('TextGenerator');
+    const [showContact, setShowContact] = useState(false);
 
-    const showSection = (section: string) => {
-        console.log(section);
-        setIsVisible(prevState => {
-            const updatedState = { ...prevState };
-            Object.keys(updatedState).forEach(key => {
-                if (key === section) {
-                    updatedState[key] = true;
-                } else {
-                    updatedState[key] = false;
-                }
-            });
-            return updatedState;
-        });
+    const showSection = (section) => {
+        setCurrentSection(section);
+        // Ensure that toggling to 'TextGenerator' from 'contact' resets the contact info display
+        if (section === 'TextGenerator' && showContact) {
+            setShowContact(false);
+        }
     };
+
+    const toggleContactInfo = () => {
+        // This ensures that toggling contact info is only relevant when in the TextGenerator section
+        if (currentSection === 'TextGenerator') {
+            setShowContact(!showContact);
+        }
+    };
+
     
     const openResume = () => { 
-
         window.open('../../assets/Resume.pdf', '_blank');
     } 
 
+
     return (
         <>
-            { !isVisible.xterm && (
+            { currentSection !== 'xterm' && (
             <>
                 <div className="absolute navigation-links top-0 left-0 m-4" style={{ display: 'flex', gap: '20px'}}>
                 <button onClick={() => showSection('xterm')} style={{color:"white", cursor:"pointer", fontFamily:'vt323', fontSize:'2.5vh'}}>.xterm()</button>
@@ -48,23 +46,24 @@ export function WebsiteManager() {
 
                 <div className="absolute navigation-links top-0 right-0 m-4" style={{ display: 'flex', gap: '20px'}}>
                 <button onClick={() => showSection('TextGenerator')} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>.about()</button>
-                    <button onClick={() => showSection('projects')} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>.projects()</button>
-                <button onClick={openResume} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>.resume()</button> 
+                <button onClick={toggleContactInfo} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>.contact()</button>
+                <button onClick={() => showSection('projects')} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>.projects()</button>
+                <button onClick={openResume} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>.resume()</button>
                 
                     <a href={gitHubProfileUrl} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>
                         <GitHubIcon className="w-6 h-6" />
                     </a>
                     <a href={linkedInProfileUrl} style={{ color: 'white', cursor: 'pointer', fontFamily:'vt323', fontSize:'2.5vh' }}>
-                    <LinkedInIcon className="w-6 h-6"/>
+                        <LinkedInIcon className="w-6 h-6"/>
                     </a>
                 </div>
             </>
             )}
 
         
-            {isVisible.xterm && <Xterm className="h-[50rem] w-full dark:bg-black bg-black  bg-dot-white/[0.3] relative flex items-center justify-center" />}
-            {isVisible.TextGenerator && <TextGenerator className="dark:bg-black bg-dot-white/[0.3]  relative font-bold z-20 text-pretty justify-center"/>}
-            {isVisible.projects && <div className="w-full h-[50rem] flex items-center justify-center"><Projects/></div>}
+            {currentSection === 'xterm' && <Xterm className="h-[50rem] w-full dark:bg-black bg-black bg-dot-white/[0.3] relative flex items-center justify-center" />}
+            {currentSection === 'TextGenerator' && <TextGenerator className="dark:bg-black bg-dot-white/[0.3] relative font-bold z-20 text-pretty justify-center" showContact={showContact} />}
+            {currentSection === 'projects' && <div className="w-full h-[50rem] flex items-center justify-center"><Projects /></div>}
         </>
     );
 }
--- src/components/ui/TextGenerator.tsx
+++ src/components/ui/TextGenerator.tsx
@@ -1,18 +1,33 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import TypeWriterEffect from 'react-typewriter-effect';
 import '../../index.css';
-import {ReactComponent as LinkedInIcon} from '../../assets/icons/linkedin.svg';
-import {ReactComponent as GitHubIcon} from '../../assets/icons/github1.svg';
+import { ReactComponent as LinkedInIcon } from '../../assets/icons/linkedin.svg';
+import { ReactComponent as GitHubIcon } from '../../assets/icons/github1.svg';
 
 const linkedInProfileUrl = "https://www.linkedin.com/in/diegocoronado0/";
 const gitHubProfileUrl = "https://www.github.com/sudoneoox";
 
-export const TextGenerator = ({ className}) => {
+export const TextGenerator = ({ className, showContact }) => {
   const [displayString, setDisplayString] = useState("");
   const [initialBuffer, setInitialBuffer] = useState("");
-
+  const contactInfo = `Feel free to reach out to me with any questions or business inquiries. I'm always open to new opportunities and meeting new people. Let's connect!
+  
+  Email: diegoa2992@gmail.com
+  LinkedIn: ${linkedInProfileUrl}
+  GitHub: ${gitHubProfileUrl}`;
+  const [phase, setPhase] = useState('typing'); // typing, deleting, contactInfo
   const containerRef = useRef<HTMLDivElement>(null);
   const appendFlagRef = useRef(true);
+
+
+
+  useEffect(() => {
+    if(showContact === true) {
+      setPhase('deleting');
+    } else {
+      setPhase('typing');
+    }
+  }, [showContact]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -27,18 +42,34 @@ export const TextGenerator = ({ className}) => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+
+
   const handleTextUpdate = useCallback(() => {
-    if (appendFlagRef.current && containerRef.current) {
-      const chunkSize = 50;
-      const newDisplayString = initialBuffer.substring(0, displayString.length + chunkSize);
-      setDisplayString(newDisplayString);
+    if(phase === 'typing') {
+      if (appendFlagRef.current && containerRef.current) {
+        const chunkSize = 50;
+        const newDisplayString = initialBuffer.substring(0, displayString.length + chunkSize);
+        setDisplayString(newDisplayString);
+      } else {
+        const chunkSize = 50;
+        setDisplayString((currentDisplay) => {
+          const start = currentDisplay.substring(chunkSize);
+          const end = initialBuffer.substring(start.length, start.length + chunkSize);
+          return start + end;
+        });
+      }
+    } else if (phase == 'deleting'){
+      if (displayString.length > 0) {
+        setDisplayString((currentDisplay) => {
+          return currentDisplay.substring(0, currentDisplay.length - 200);
+        });
+      } else {
+        setPhase('contactInfo');
+      }
     } else {
-      const chunkSize = 50;
-      setDisplayString((currentDisplay) => {
-        const start = currentDisplay.substring(chunkSize);
-        const end = initialBuffer.substring(start.length, start.length + chunkSize);
-        return start + end;
-      });
+      const chunkSize = 10;
+      const contactinfosubs = contactInfo.substring(0, displayString.length + chunkSize);
+      setDisplayString(contactinfosubs);
     }
   }, [displayString, initialBuffer]);
 
@@ -55,41 +86,50 @@ export const TextGenerator = ({ className}) => {
     return () => cancelAnimationFrame(intervalId);
   }, [handleTextUpdate]);
 
+
   return (
     <>
-    <div ref={containerRef} className={`${className}`}  style={{ fontFamily: 'vt323', fontSize: '2.5vh'}}>
-      <p className="" style={{ color: '#174d25'}}>
-        {displayString}
-      </p>
-    </div>
 
-    <div className='hello-im-tag z-20 text-white absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-    <TypeWriterEffect
-      textStyle={{ fontFamily: 'vt323', fontSize: '7.0vh', color: 'white'}}
-      startDelay={100}
-      cursorColor="white"
-      multiText={[
-        'Hello, I\'m Diego',
-        'I\'m a student',
-        'I\'m a software engineer',
-        'I\'m a problem solver',
-        'I\'m a team player',
-        'Hello, I\'m Diego'
-      ]}
-      typeSpeed={100}
-      hideCursorAfterText={true}
-    />
-    </div>
+
+      {/* GENERATED CODE  */}
+      <div ref={containerRef} className={`${className}`} style={{ fontFamily: 'vt323', fontSize: '2.5vh' }}>
+        {phase !== 'contactInfo' ? (
+        <p className="" style={{ color: '#174d25' }}>{displayString}</p>
+      ) : (
+        <pre style={{ color: 'red', fontFamily: 'vt323', fontSize: '3.5vh' }}>{displayString}</pre>
+      )}
+      </div>
+
+
+
+
+        {/* TYPEWRITER MIDDLE OF PAGE */}
+      <div className='hello-im-tag z-20 text-white absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+        <TypeWriterEffect
+          textStyle={{ fontFamily: 'vt323', fontSize: '7.0vh', color: 'white' }}
+          startDelay={100}
+          cursorColor="white"
+          multiText={[
+            'Hello, I\'m Diego',
+            'I\'m a student',
+            'I\'m a software engineer',
+            'I\'m a problem solver',
+            'I\'m a team player',
+            'Hello, I\'m Diego'
+          ]}
+          typeSpeed={100}
+          hideCursorAfterText={true}
+        />
+      </div>
     </>
   );
 };
 
-let generateRandomString = (length: number) => {
+const generateRandomString = (length: number) => {
   const characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}[]|;:,.<>?/`~";
-  let result:string= "";
+  let result: string = "";
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
 };
-
