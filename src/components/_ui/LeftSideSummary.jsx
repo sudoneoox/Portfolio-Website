03e8ea6815d54c7bc5b11365c8538862fd9f5b80
diego@X0 Desktop <diegoa2992@gmail.com>
2025-01-28 13:52:41 -0600

    hyperlink fix

--- src/components/_ui/LeftSideSummary.jsx
+++ src/components/_ui/LeftSideSummary.jsx
@@ -34,7 +34,7 @@ const LeftSideSummary = () => {
           <SidebarLink
             icon={School}
             text={PERSONAL_INFO.university}
-            href="https:/uh.edu/nsm/computer-science/"
+            href="https://uh.edu/nsm/computer-science"
           />
           <SidebarLink
             icon={Mail}
